19c2223ef977e8e9b1288657437a9b7c86ebff08
huh4t398th489fjjf43 <150632928+huh4t398th489fjjf43@users.noreply.github.com>
2023-11-17 10:32:07 +0900

    add fm

--- src/core/sound.cpp
+++ src/core/sound.cpp
@@ -1,34 +1,41 @@
+#include <math.h>
+
 SDL_AudioSpec Desired;
 SDL_AudioSpec Obtained;
+double double Total_time = 0;
 
-double generateFMWave(double t)
+float sind(float theta) {
+    return sin(theta*2*M_PI);
+}
+
+double generateFMWave(double t, int addr)
 {
-    float f1 = (float)ram_peek(ram, 0x10000)*256+ram_peek(ram, 0x10001).toInt();
-    float v1 = ((float)ram_peek(ram, 0x10002).toInt())/255;
-    float f2 = ((float)ram_peek(ram, 0x10003).toInt())/16;
-    float v2 = ((float)ram_peek(ram, 0x10004).toInt())/255;
-    float f3 = ((float)ram_peek(ram, 0x10005).toInt())/16;
-    float v3 = ((float)ram_peek(ram, 0x10006).toInt())/255;
-    float f4 = ((float)ram_peek(ram, 0x10007).toInt())/16;
-    float v4 = ((float)ram_peek(ram, 0x10008).toInt())/255;
+    float f1 = ((float)ram_peek(ram, addr+0)*256+ram_peek(ram, addr+1).toInt());
+    float v1 = ((float)ram_peek(ram, addr+2).toInt())/255;
+    float f2 = ((float)ram_peek(ram, addr+3).toInt())/16;
+    float v2 = ((float)ram_peek(ram, addr+4).toInt())/255;
+    float f3 = ((float)ram_peek(ram, addr+5).toInt())/16;
+    float v3 = ((float)ram_peek(ram, addr+6).toInt())/255;
+    float f4 = ((float)ram_peek(ram, addr+7).toInt())/16;
+    float v4 = ((float)ram_peek(ram, addr+8).toInt())/255;
+    double value = sind(t*f1+sind(t*f1*f2+sind(t*f1*f3+sind(t*f1*f4)*v4)*v3)*v2)*v1*127+127;
 }
 
 void callback(void *unused, Uint8 *stream, int len)
 {
     int i;
-    static unsigned int step= 0;
     Uint16 *frames = (Uint16 *) stream;
     int framesize = len / 2;
-    for (i = 0; i < framesize; i++, step++) {
-        frames[i]= generateFMWave(step * Frequency / Obtained.freq) * 3000;
+    for (i = Total_time; i < framesize + Total_time; i++, Total_time++) {
+        frames[i]= generateFMWave(Total_time / Obtained.freq);
     }
 }
 
 void playSound() {
-    Desired.freq= 22050; /* Sampling rate: 22050Hz */
+    Desired.freq= 48000; /* Sampling rate: 48000Hz */
     Desired.format= AUDIO_S16LSB; /* 16-bit signed audio */
     Desired.channels= 0; /* Mono */
-    Desired.samples= 8192; /* Buffer size: 8K = 0.37 sec. */
+    Desired.samples= 512; /* Buffer size: 512 */
     Desired.callback= callback;
     Desired.userdata= NULL;
 
