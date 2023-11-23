2b920003589e4b9c0e5ba36c489415909520ebde
src3453 <info@example.com>
2023-11-23 20:30:29 +0900

    some

--- src/lua/tracker.lua
+++ src/lua/tracker.lua
@@ -2,6 +2,20 @@ mode=0
 cur0=0
 cur1=0
 
+showcur(0)
+startinput()
+modeLabel = {
+    "Track",
+    "Pattern",
+    "Insturument",
+}
+
+function note2freq(n)
+    concert_pitch = 440;
+    f = concert_pitch * 2 ^ ((n - 69)/12)
+    return int(f)
+end
+
 function TrackEditor()
     print("FRM FM1 FM2 FM3 FM4 WT5 WT6",16,4,rgb(192,255,192))
     rectb(15,19+(cur0//6%16)*16,209,12,250)
@@ -31,8 +45,11 @@ function PatternEditor()
             print(string.sub(notes,note*2%24+1,note*2%24+2)..note//12,48,20+y*16,rgb(255,255,255))
         end
         note=peek(cur0*256+y*4+1)
-        print(string.format("%02X",note),96,20+y*16,rgb(255,255,255))
+        print(string.format("%02X",note),88,20+y*16,rgb(255,255,255))
     end
+end
+
+function InstEditor()
 end
 
 function drawcur()
@@ -50,12 +67,6 @@ function drawcur()
     
 end
 
-showcur(0)
-startinput()
-modeLabel = {
-    "Track",
-    "Pattern",
-}
 function LOOP()
     cls(0)
     rect(0,275,384,13,255)
@@ -65,6 +76,10 @@ function LOOP()
     end
     if mode == 1 then
         PatternEditor()
+        print(modeLabel[mode+1].." "..string.format("%02X",cur0),1,276,0)
+    end
+    if mode == 2 then
+        InstEditor()
         print(modeLabel[mode+1].." "..string.format("%02X",cur0),1,276,0)
     end  
     drawcur()
@@ -126,15 +141,15 @@ function ONKEYDOWN(k)
             cur1=(cur1+1)%256
         end
         if to_key_name(k) == "S" then
-            cur0=(cur0-1)%128
+            cur0=(cur0-1)%256
         end
         if to_key_name(k) == "X" then
-            cur0=(cur0+1)%128
+            cur0=(cur0+1)%256
         end
     end
     if to_key_name(k) == "Z" then
 
-        mode=(mode+1)%2
+        mode=(mode+1)%3
 
     end
 end
